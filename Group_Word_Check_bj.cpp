fc820cb04d4fd974419ec323e652a19038baa93c
Kim Jeong Jin <kjjgo35@gmail.com>
2019-09-21 22:29:21 +0900

    Group_Word_Check - Baekjoon(1316) - Success

---
+++ Group_Word_Check_bj.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int N, n_count, length;
+string word;
+int wordcheck[26];
+bool key;
+
+int main() {
+	cin >> N;
+	for (int i{ 0 }; i < N; i++) {
+		key = true;
+		cin >> word;
+		length = word.length();
+		for (int j{ 0 }; j < length; j++) {
+			if (j > 0) {
+				if (word[j] != word[j - 1]) {
+					for (int k{ 0 }; k < j-1; k++) {
+						if (word[k] == word[j]) {
+							key = false;
+							break;
+						}
+					}
+				}
+			}
+		}
+		if (key == true)
+			n_count++;
+	}
+	cout << n_count << endl;
+}
