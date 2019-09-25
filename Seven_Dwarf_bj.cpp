71b5630199abb5a586bf2dd7f9528b4c0d691506
Kim Jeong Jin <kjjgo35@gmail.com>
2019-09-25 23:53:02 +0900

    Seven_Dwarf - Baekjoon(2309) - Use Next_Permutation - Success

--- Seven_Dwarf_bj.cpp
+++ Seven_Dwarf_bj.cpp
@@ -2,36 +2,51 @@
 // Brute Force
 #include <iostream>
 #include <string>
+#include <vector>
 #include <algorithm>
 
 using namespace std;
 
-int save1, save2, n_count;
+int save, height;
 int sum;
-int before[9];
-int after[7];
+vector<int>before;
+vector<int>v_select;
+vector<int>after;
+
+void next_per_combination(int k) {
+	for (int i = 0; i < k; i++) {
+		v_select.push_back(1);
+	}
+	for (int i = 0; i < before.size() - k; i++) {
+		v_select.push_back(0);
+	}
+	sort(v_select.begin(), v_select.end());
+	do {
+		save = 0;
+		for (int i = 0; i < before.size(); i++) {
+			if (v_select[i] == 1)
+				save += before[i];
+		}
+		if (save + 100 == sum) {
+			for (int i = 0; i < before.size(); i++) {
+				if (v_select[i] == 0)
+					after.push_back(before[i]);
+				
+			}
+			break;
+		}
+	} while (next_permutation(v_select.begin(), v_select.end()));
+}
 
 int main() {
 	for (int i{ 0 }; i < 9; i++) {
-		cin >> before[i];
+		cin >> height;
+		before.push_back(height);
 		sum += before[i];
 	}
-	for (int i{ 0 }; i < 9; i++) {
-		for (int j{ i + 1 }; j < 9; j++) {
-			if (sum - (before[i] + before[j]) == 100) {
-				save1 = i;
-				save2 = j;
-				break;
-			}
-		}
-	}
-	for (int i{ 0 }; i < 9; i++) {
-		if (i != save1 && i != save2) {
-			after[n_count] = before[i];
-			n_count++;
-		}
-	}
-	sort(after, after+7);
+	next_per_combination(2);
+	
+	sort(after.begin(), after.end());
 	for (int i{ 0 }; i < 7; i++) {
 		cout << after[i] << "\n";
 	}
